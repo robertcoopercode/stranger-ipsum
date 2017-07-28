2e77baca1090ee6d0b3231fda4bb1ac9773328ab
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-28 13:19:58 -0400

    Generate sentences between 8 and 12 words in lenght

--- generator.js
+++ generator.js
@@ -6,7 +6,7 @@ const loremIpsum = new GenerateNewText();
 // Constructor function creating the GenerateNewText object prototype
 function GenerateNewText() {
   // Add new properties to the prototype
-  this.words = ["lorem", "ipsum", "robert", "created", "this", "apple", "airpods", "apple watch", "ios", "samsung"];
+  this.words = ["technology", "Apple", "Samsung", "Nokia", "computer", "MacBook", "iOS", "Android", "iPhone", "Galaxy", "AirPods", "Apple Watch", "Magic Mouse", "Google", "Amazon", "AWS", "WordPress", "Node.js", "Angular.js", "Framework", "CSS", "HTML", "JavaScript", "SASS", "Treehouse", "Code Academy", "Coding"];
 }
 
 // Method that generates a random word
@@ -18,10 +18,12 @@ GenerateNewText.prototype.getRandomWord = function() {
 // Method that generates a sentence
 GenerateNewText.prototype.getSentence = function() {
   let sentence = [];
-  while (sentence.length < 10) {
+  // Make the length of the sentence between 8 and 12 words long
+  let sentenceLength = Math.floor(Math.random() * 5) + 8
+  while (sentence.length < sentenceLength) {
     sentence.push(this.getRandomWord());
   }
   return sentence;
 }
 
-console.log(loremIpsum.getSentence());
+console.log(loremIpsum.getSentence().length);
