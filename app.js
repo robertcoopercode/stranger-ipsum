95d30f4e2da73eed22049403b0e7fe70cf4834c1
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-28 11:55:32 -0400

    Created a web server

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Problem: We need a way to generator paragraphs of random dummy text
-// Solution: Use Node.js to take the number of paragraphs as inputs and then serve our template via HTTP 
+// Solution: Use Node.js to take the number of paragraphs as inputs and then serve our template via HTTP
 
-// Create server
+const http = require('http');
+const hostname = "127.0.0.1";
+const port = 3000;
+
+// Create a web server
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+});
 
 // Listen to port 3000
