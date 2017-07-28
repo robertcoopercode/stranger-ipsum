b85946e190840612eaa3c4ac5e54c26a9a8c9a76
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-28 11:48:32 -0400

    Outline what is required in app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// Problem: We need a way to generator paragraphs of random dummy text
+// Solution: Use Node.js to take the number of paragraphs as inputs and then serve our template via HTTP 
+
+// Create server
+
+// Listen to port 3000
