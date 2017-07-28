00a734767cf22e27d621582eebc24a43ea2cf92f
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-28 13:08:24 -0400

    Generate a sentence with random words

--- generator.js
+++ generator.js
@@ -9,9 +9,19 @@ function GenerateNewText() {
   this.words = ["lorem", "ipsum", "robert", "created", "this", "apple", "airpods", "apple watch", "ios", "samsung"];
 }
 
+// Method that generates a random word
 GenerateNewText.prototype.getRandomWord = function() {
   let randomWord = this.words[Math.floor(Math.random() * this.words.length)]
 	return randomWord;
 }
 
-console.log(loremIpsum.getRandomWord());
+// Method that generates a sentence
+GenerateNewText.prototype.getSentence = function() {
+  let sentence = [];
+  while (sentence.length < 10) {
+    sentence.push(this.getRandomWord());
+  }
+  return sentence;
+}
+
+console.log(loremIpsum.getSentence());
