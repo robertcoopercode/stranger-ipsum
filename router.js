44c775fe1eae5f7797678aaa8002dc60ee2f70b5
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-29 12:30:18 -0400

    Got the lorem ipsum generator and writing to the page (not as HTML)

--- router.js
+++ router.js
@@ -1,13 +1,12 @@
-// const generator = require("./generator.js");
+const generator = require("./generator.js");
 
 let commonHeaders = ['Content-Type', 'text/html'];
 
 function router(request, response) {
   // If URL == "/" && GET request
   if (request.url === "/" && request.method.toLowerCase() === "get") {
-    console.log("This worked and we are on the home page");
     response.setHeader(commonHeaders[0], commonHeaders[1]);
-    response.write("Home Page :D");
+    response.write(generator.loremIpsum.getAllParagraphs());
     response.end();
   };
 }
