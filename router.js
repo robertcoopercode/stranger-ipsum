c559a203fb6ba390939e31b10f7f3e4ba176fd38
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-30 00:01:10 -0400

    Input value submitted by user is now used to display to correct number of paragraphs

--- router.js
+++ router.js
@@ -1,15 +1,30 @@
 // Require the necessary modules
 const generator = require("./generator.js");
 const renderer = require("./renderer.js");
+const querystring = require("querystring");
 
 function router(request, response) {
-  // If URL == "/" && GET request
-  if (request.url === "/" && request.method.toLowerCase() === "get") {
-    let loremIpsumText = generator.loremIpsum.getAllParagraphs(2);
-    response.setHeader('Content-Type', 'text/html');
-    renderer.renderLoremIpsum("index", loremIpsumText , response);
-    response.end();
-  };
-}
+
+  if (request.url === "/") {
+    // If URL == "/" && GET
+    if (request.method.toLowerCase() === "get") {
+      response.setHeader('Content-Type', 'text/html');
+      renderer.view("index", response);
+      response.end();
+    } else {
+    // Else the URL == "/" && POST
+      console.log("The submit buttom got pressed");
+      // Get the POST data from input
+      request.on("data", function(inputValue) {
+        let query = inputValue.toString();
+        let numberOfParagraphs = querystring.parse(query).numberOfParagraphs;
+        let loremIpsumText = generator.loremIpsum.getAllParagraphs(numberOfParagraphs);
+        response.setHeader('Content-Type', 'text/html');
+        renderer.renderLoremIpsum("index", loremIpsumText , response);
+        response.end();
+      });
+    }
+  }
+};
 
 module.exports.router = router;
