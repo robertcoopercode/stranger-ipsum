4f9be4ee57eedd6c01758bf5ba8d6b909b6fec1b
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-29 23:53:23 -0400

    Created a new view function to display the home page

--- renderer.js
+++ renderer.js
@@ -1,10 +1,16 @@
-// Require the necessary modules 
+// Require the necessary modules
 var fs = require("fs");
 
 function renderLoremIpsum(templateName, text, response) {
-  var fileContents = fs.readFileSync("./views/" + templateName + ".html", {encoding: "utf8"});
+  let fileContents = fs.readFileSync("./views/" + templateName + ".html", {encoding: "utf8"});
   fileContents = fileContents.replace("<div class='lorem-ipsum-container'></div>",text);;
   response.write(fileContents);
 }
 
+function view(templateName, response) {
+  let fileContents = fs.readFileSync("./views/" + templateName + ".html", {encoding: "utf8"});
+  response.write(fileContents);
+}
+
 module.exports.renderLoremIpsum = renderLoremIpsum;
+module.exports.view = view;
