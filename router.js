388869cb10f57b3cb3154b6e104318b03ce5ca5f
Robert Cooper <robert.cooper.qu@gmail.com>
2017-07-30 01:13:01 -0400

    Seperated header into it's own file

--- router.js
+++ router.js
@@ -9,17 +9,18 @@ function router(request, response) {
     // If URL == "/" && GET
     if (request.method.toLowerCase() === "get") {
       response.setHeader('Content-Type', 'text/html');
+      renderer.view("header", response);
       renderer.view("index", response);
       response.end();
     } else {
     // Else the URL == "/" && POST
-      console.log("The submit buttom got pressed");
       // Get the POST data from input
       request.on("data", function(inputValue) {
         let query = inputValue.toString();
         let numberOfParagraphs = querystring.parse(query).numberOfParagraphs;
         let loremIpsumText = generator.loremIpsum.getAllParagraphs(numberOfParagraphs);
         response.setHeader('Content-Type', 'text/html');
+        renderer.view("header", response);
         renderer.renderLoremIpsum("index", loremIpsumText , response);
         response.end();
       });
