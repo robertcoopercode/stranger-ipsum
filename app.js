b2f09328f854264192bae2c2cc09744501606558
Robert Cooper <robert.cooper.qu@gmail.com>
2017-09-04 20:41:29 -0400

    Added the ability to copy text using a button

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const server = http.createServer((request, response) => {
   response.statusCode = 200;
   // Serve the appropriate result by calling the router function inside the router module
   router.router(request, response);
-
 });
 
 // Begin accepting connections to the specified port
