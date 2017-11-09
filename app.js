cc5e0ca90c3126ef4d303f40421fd005754ff5fc
Robert Cooper <robert.cooper.qu@gmail.com>
2017-11-09 12:33:51 -0500

    Using Express in the app

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-// Problem: We need a way to generator paragraphs of random dummy text
-// Solution: Use Node.js to take the number of paragraphs as an input from
-// the user and then serve the result via HTTP
-
 // Require necessary modules
 const http = require('http');
-const router = require('./router.js');
-
+const express = require('express');
+const app = express();
+const router = require('./router.js').router;
 // Define the hostname and port where the server can be found
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
 
-// Create a web server
-const server = http.createServer((request, response) => {
-  // If the server was created successfully, the statusCode will be set to 200
-  response.statusCode = 200;
-  // Serve the appropriate result by calling the router function inside the router module
-  router.router(request, response);
+// Define the directory where static files are found
+app.use(express.static('public'));
+
+app.use( (req, res, next) => {
+  router(req, res);
 });
 
 // Begin accepting connections to the specified port
-server.listen(port, () => {
+app.listen(port, () => {
   // Display server location information to the console
   console.log(`Server is listening at http://${hostname}:${port}/`);
 })
