889b6a5146591130809931fb99da5d3406ed7012
Robert Cooper <robert.cooper.qu@gmail.com>
2017-11-10 19:36:43 -0500

    Using Express for the application routing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// Require necessary modules
-const http = require('http');
+// Require express and create an express application instance
 const express = require('express');
 const app = express();
-const router = require('./router.js').router;
+
+// Require the express routes defined in router.js
+const routes = require('./router');
+
 // Define the hostname and port where the server can be found
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
@@ -10,9 +12,8 @@ const port = process.env.PORT || 3000;
 // Define the directory where static files are found
 app.use(express.static('public'));
 
-app.use( (req, res, next) => {
-  router(req, res);
-});
+// Specify the routes to be used for our application 
+app.use(routes);
 
 // Begin accepting connections to the specified port
 app.listen(port, () => {
--- router.js
+++ router.js
@@ -2,44 +2,40 @@
 const generator = require("./generator.js");
 const querystring = require("querystring");
 const fs = require("fs");
-
 const path = require('path');
 
-// Location of your favicon in the filesystem.
+// Require express and create an express router object
+const express = require('express');
+const router = express.Router();
+
+// Location of the favicon in our directory 
 const FAVICON = path.join(__dirname, 'favicon.ico');
 
-function router(request, response) {
+router.get('/favicon.ico', (request, response) => {
+  response.setHeader('Content-Type', 'image/x-icon');
+  let fileContents = fs.readFileSync(FAVICON);
+  response.write(fileContents, {encoding: "utf8"});
+  response.end();
+});
 
-  // Load the Favicon
-  if (request.method.toLowerCase() === "get" && request.url === '/favicon.ico') {
-    response.setHeader('Content-Type', 'image/x-icon');
-    let fileContents = fs.readFileSync(FAVICON);
-    response.write(fileContents, {encoding: "utf8"});
+router.get('/', (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
+  response.write(fileContents);
+  response.end();
+});
+
+router.post('/', (request, response) => {
+  request.on("data", function(inputValue) {
+    let query = inputValue.toString();
+    let numberOfParagraphs = querystring.parse(query).numberOfParagraphs;
+    let loremIpsumText = generator.loremIpsum.getAllParagraphs(numberOfParagraphs);
+    let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
+    fileContents = fileContents.replace("<div class='placeholder-div'></div>",loremIpsumText);;
+    response.setHeader('Content-Type', 'text/html');
+    response.write(fileContents);
     response.end();
-  }
+  });
+});
 
-  if (request.url === "/") {
-    // If URL == "/" && GET
-      if (request.method.toLowerCase() === "get") {
-      response.setHeader('Content-Type', 'text/html');
-      let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
-      response.write(fileContents);
-      response.end();
-    } else {
-    // Else the URL == "/" && POST
-      // Get the POST data from input
-      request.on("data", function(inputValue) {
-        let query = inputValue.toString();
-        let numberOfParagraphs = querystring.parse(query).numberOfParagraphs;
-        let loremIpsumText = generator.loremIpsum.getAllParagraphs(numberOfParagraphs);
-        let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
-        fileContents = fileContents.replace("<div class='placeholder-div'></div>",loremIpsumText);;
-        response.setHeader('Content-Type', 'text/html');
-        response.write(fileContents);
-        response.end();
-      });
-    }
-  }
-};
-
-module.exports.router = router;
+module.exports = router;
