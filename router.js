72eb1bca28bd041f6d42e1fdc685f862a4a053ca
Robert Cooper <robert.cooper.qu@gmail.com>
2018-01-13 13:02:37 -0500

    Add comments to router.js

--- router.js
+++ router.js
@@ -11,6 +11,7 @@ const router = express.Router();
 // Location of the favicon in our directory
 const FAVICON = path.join(__dirname, 'favicon.ico');
 
+// Route that serves the application Favicon
 router.get('/favicon.ico', (request, response) => {
   response.setHeader('Content-Type', 'image/x-icon');
   let fileContents = fs.readFileSync(FAVICON);
@@ -18,21 +19,32 @@ router.get('/favicon.ico', (request, response) => {
   response.end();
 });
 
+// Route that serves index.html
 router.get('/', (request, response) => {
   response.setHeader('Content-Type', 'text/html');
+  // Capture the contents of index.html in a variable
   let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
+  // Send a response to the client with the index.html file
   response.write(fileContents);
   response.end();
 });
 
+// Route that generates the lorem ipsum text and reloads a modified index.html
 router.post('/', (request, response) => {
   request.on("data", function(inputValue) {
-    let query = inputValue.toString();
+    // Convert the POST data into a readable string
+    let query = inputValue.toString(); // i.e. numberOfParagraphs=3
+    // Parse the query into a key/value pair and store the value of numberOfParagraphs
+    // in a variable
     let numberOfParagraphs = querystring.parse(query).numberOfParagraphs;
+    // Generate the lorem ipsum text with the getAllParagraphs function
     let loremIpsumText = loremIpsum.getAllParagraphs(numberOfParagraphs);
+    // Capture the contents of index.html in a variable
     let fileContents = fs.readFileSync("./public/index.html", {encoding: "utf8"});
+    // Replace the placeholder <div> with the lorem ipsum text
     fileContents = fileContents.replace("<div class='placeholder-div'></div>",loremIpsumText);;
     response.setHeader('Content-Type', 'text/html');
+    // Send a response to the client with the modified index.html file
     response.write(fileContents);
     response.end();
   });
